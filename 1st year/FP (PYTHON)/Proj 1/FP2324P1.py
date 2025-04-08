8dd3cdd781663f1f6cfcb1724aa86e63269b859c
Tomas Ferreira <tomasmartinsferreira@tecnico.ulisboa.pt>
2025-04-08 13:18:41 +0100

    test

--- 1st year/FP (PYTHON)/Proj 1/FP2324P1.py
+++ 1st year/FP (PYTHON)/Proj 1/FP2324P1.py
@@ -1,11 +1,3 @@
-"""
-Grade: 18.25 out of 20 (SOLO PROJECT)
-In this 1st Programming Fundamentals project,
-create functions to analyze a rectangular territory made of intersecting paths,
-which may contain mountains forming ranges and valleys.
-"""
-# ========== START OF CODE ==========
-
 def eh_territorio(territorio):
     '''
     Verifica se o territorio está correto verificando o seu type e das suas linhas e elementos,
---
+++ 1st year/FP (PYTHON)/Proj 1/README.md
@@ -0,0 +1,4 @@
+Grade: 18.25 out of 20 (SOLO PROJECT)
+In this 1st Programming Fundamentals project,
+create functions to analyze a rectangular territory made of intersecting paths,
+which may contain mountains forming ranges and valleys.
